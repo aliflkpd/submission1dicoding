548b62cfdde645a253a0d2aa8df670131861f66b
Aliflkpd <andialifnurman@gmail.com>
2022-09-20 02:13:22 +0800

    first commit

---
+++ lib/models/babylist.dart
@@ -0,0 +1,159 @@
+class BabyDomination {
+  String baby;
+  String priceBaby;
+  String descriptionBaby;
+  String babyImage;
+  String ethereumImage;
+
+  BabyDomination({
+    required this.baby,
+    required this.priceBaby,
+    required this.descriptionBaby,
+    required this.babyImage,
+    required this.ethereumImage,
+  });
+}
+class ForLike()
+
+var babyList = [
+  BabyDomination(
+    baby: 'baby#01',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/alifwhite.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#02',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/evos.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#03',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/datamine.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#04',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/bandaged.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#05',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/kawaki.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#06',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/jefri.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#07',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/pinkninjagirl.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#08',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/catonthehead.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#09',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/gravityninja.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#10',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/lingard.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#11',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/goku.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#12',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/zuko.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#13',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/sukuna.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#14',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/akaza.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#15',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/waterboy.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#16',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/pandkarafuru-nojacket.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#17',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/flame.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#18',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/deddy.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#19',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/hellboy.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+  BabyDomination(
+    baby: 'baby#20',
+    priceBaby: '2',
+    descriptionBaby: 'test',
+    babyImage: 'images/pinkcrop.jpg',
+    ethereumImage: 'images/eth.png',
+  ),
+];
